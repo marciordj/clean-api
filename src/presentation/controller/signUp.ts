ddfcfef7831e4a72b74ab912b225a1a78af3376b
Marcio Rodrigues <luiz.rodrigues@eduzz.com>
2020-10-24 16:42:18 -0300

    feat: retornar 400 se o email nao for fornecido

--- src/presentation/controller/signUp.ts
+++ src/presentation/controller/signUp.ts
@@ -1,8 +1,31 @@
+// interface IRequest {
+//   body: {
+//     name: any
+//     email: any
+//     password: any
+//     passwordConfirmation: any
+//   }
+// }
+
+// interface IResponse {
+//   statusCode: number
+//   body: any
+// }
+
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
     }
   }
 }
